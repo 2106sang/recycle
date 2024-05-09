69652fa91904557bbba12d98de0e9084234fbfa1
2106sang <76397656+2106sang@users.noreply.github.com>
2024-05-09 16:35:36 +0700

    Create cloudflare.sh

---
+++ cloudflare.sh
@@ -0,0 +1,5 @@
+curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && 
+
+sudo dpkg -i cloudflared.deb && 
+
+sudo cloudflared service install eyJhIjoiYzMzOTNjMzRjZmUxNDQ5NmQ0ZDJmZDMzNWJlZDU3NDciLCJ0IjoiZTQyZTgwNDYtMGRlOS00NGYzLWEyMmYtZDA5MDFjOTQ3NTcxIiwicyI6Ill6TTJaamxrTlRFdE4yVmxNQzAwT1dVM0xXSmtNREV0WXpVMlpEZGtPVE0xTnpoaiJ9
